aa1b621a01a04e17f2b753665e3cd90d77e1aa83
btbenedi <btbenedi@gmail.com>
2019-05-23 23:24:49 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta author="InMotion Hosting" />
+		<title>BAMS - Site is Under Construction</title>
+		<link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic' rel='stylesheet' type='text/css'>
+		<style type="text/css">
+			body {
+				background: #fcfaf2;
+				font-family: Vollkorn;
+				padding-top: 100px;
+				text-align: center;
+			}
+
+			#heading {
+				color: #353535;
+				font: italic normal normal 72px 'Gentium Book Basic';
+			}
+
+			p {
+				border-bottom: 1px solid #e2e1d9;
+				border-top: 1px solid #e2e1d9;
+				color: #c90707;
+				font: normal normal normal 18px 'Gentium Book Basic', serif;
+				margin: 10px auto 0;
+				padding: 20px 0;
+				width: 750px
+			}
+
+			#logo {
+				display: block;
+				margin: 0 auto 10px;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div id="heading">
+			Under Construction
+		</div>
+		<p>
+			We apologize for any inconvenience and should be back up with a newly updated website soon.
+		</p>
+	</body>
+</html>
